446bbbf84aff0f5d50d148509e93a3641c0b0165
Don Casanova <ethanjuan@yahoo.com>
2019-02-07 14:48:41 -0600

    test

--- js/pic_dynamic.js
+++ js/pic_dynamic.js
@@ -1,10 +1,11 @@
 var image = ["./images/dixie.jpg", "./images/dixie on a bird.jpg", "./images/food turkey 068.JPG", "./images/skiing.jpg", "./images/SnarlingWolf.jpg"];
 
 
-image.sort(function (a, b) { return 0.5 - Math.random() });
+
 
 addImage();
 function addImage() {
+    image.sort(function (a, b) { return 0.5 - Math.random() });
     for (i = 0; i < image.length; i++) {
      
         $(".insertImagesHere").append(`<div id="target" class=" col-3 imageContainer"><a href="#" class="" data-toggle="modal" data-target="#largeModal"><img class="imageStyle imageHover pointer"src= "${image[i]}" alt="${image[i]}" height="300" ></a></div>`);
@@ -16,6 +17,6 @@ function addImage() {
 
 $(document).on("click", ".imageStyle", function (image) {
     console.log("in function")
-   
-
+    $(".insertImagesHere").empty();
+    addImage();
 });
