eaea923e0a111381313202482c46f313ccfed1a1
Don Casanova <ethanjuan@yahoo.com>
2019-02-08 09:20:47 -0600

    test

--- js/pic_count.js
+++ js/pic_count.js
@@ -3,6 +3,8 @@
 //if it hasent been added we add it and then add a count to the total number clicked
 //if it has been added we add a count to the losses and then reset game
 var image = ["./images/dixie.jpg", "./images/dixie on a bird.jpg", "./images/food turkey 068.JPG", "./images/skiing.jpg", "./images/SnarlingWolf.jpg"];
+var doingWell = ["Your doing great", "Your on your way to a win", "Wow keep it going", "getting close", "You Won...Yeah!","Sorry, lets try again,"]
+var clicks = 0
 let imageArray = [];
 let losses = 0
 let count = 0
@@ -18,6 +20,7 @@ $(document).on("click", ".imageStyle", function () {
         if (findImage == -1) {
             imageArray.push(imageModal)
             count = imageArray.length
+           
             if (count == 5) {
                 //clear imageArray and add to wins then clears counts
                 imageArray.length = 0
@@ -41,8 +44,16 @@ $(document).on("click", ".imageStyle", function () {
             $("#count").empty();
             const count = 0
             $("#count").append(`<p class "text-light">Clicky Count: ${count}</p>`);
+            clicks=5
     }
    
    
 });
 
+function winningStrokes() {
+    console.log("in strokes")
+    $(".instructions").append(`<h5>
+    ${doingWell[clicks]}
+        </h5>`);
+    clicks++
+}
--- js/pic_dynamic.js
+++ js/pic_dynamic.js
@@ -1,14 +1,13 @@
 var image = ["./images/dixie.jpg", "./images/dixie on a bird.jpg", "./images/food turkey 068.JPG", "./images/skiing.jpg", "./images/SnarlingWolf.jpg"];
 
 
-
-
+addInstructions()
 addImage();
 function addImage() {
     image.sort(function (a, b) { return 0.5 - Math.random() });
     for (i = 0; i < image.length; i++) {
      
-        $(".insertImagesHere").append(`<div id="target" class=" col-3 imageContainer"><a href="#" class="" data-toggle="modal" data-target="#largeModal"><img class="imageStyle imageHover pointer"src= "${image[i]}" alt="${image[i]}" height="300" ></a></div>`);
+        $(".insertImagesHere").append(`<div id="target" class=" col-2 imageContainer"><a href="#" class="" data-toggle="modal" data-target="#largeModal"><img class="imageStyle imageHover pointer"src= "${image[i]}" alt="${image[i]}" height="300" ></a></div>`);
         var imageNumber = i;
        
 }
@@ -16,7 +15,17 @@ function addImage() {
 }
 
 $(document).on("click", ".imageStyle", function (image) {
-    console.log("in function")
+    $(".instructions").empty();
+    winningStrokes()
     $(".insertImagesHere").empty();
     addImage();
 });
+
+
+function addInstructions() {
+    console.log("inside instruction load")
+    $(".instructions").append(`<h5>
+    The object of this game is to click every image without clicking on it twice. If you click it twice the game will reset and a loss will be added to your count. If you click them all once you win and a win will be added to your count. Hope you enjoy testing your memory.
+        </h5>`);
+}
+
